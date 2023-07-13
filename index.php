48b8a25360023f8f1c220ce6b08c73a7e1787169
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-07-13 16:17:39 +0200

    added html structure

--- index.php
+++ index.php
@@ -20,3 +20,15 @@ class Movie{
 $actionFilm = new Movie('Alla ricerca del calice perduto','azione',2003);
 $horrorFilm = new Movie('Saw l\'\enigmista','horror',2010);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+    
+ </body>
+</html>
