257176364cd588dbb848e3ffe3a66c545010d8db
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-07-13 16:10:15 +0200

    added a public method for class Movie

--- index.php
+++ index.php
@@ -5,6 +5,15 @@ class Movie{
     public $genre;
     public $year;
 
-    
+    function __construct(String $_title,String $_genre, INT $_year){
+
+        $this->title = $_title;
+        $this->genre = $_genre;
+        $this->year = $_year;
+    }
+
+    public function getTitle(){
+        return $this-> title;
+    }
 }
 ?>
